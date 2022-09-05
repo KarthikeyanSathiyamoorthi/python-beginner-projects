1990ed71f67c7370c45035ddb4759afa8d42f921
karthikeyan sathiyamoorthi <88829456+KarthikeyanSathiyamoorthi@users.noreply.github.com>
2022-09-06 00:26:41 +0530

    Add files via upload

---
+++ guess-number.py
@@ -0,0 +1,35 @@
+import random  # import this one for creating random numbers between two numbers
+
+def guess(x):
+    random_num = random.randint(1, x)
+    guess = 0
+    while guess != random_num:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_num:
+            print("sorry, guess again. Too low.")
+        elif guess > random_num:
+            print("sorry, guess again. Too high.")
+    
+    print(f'Yay, congrats. you have gussed the number {random_num}')
+
+# guess(50)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != 'c':
+        if low != high:
+           guess = random.randint(low, high)
+        else:
+            guess = low #could also be high bcz low = high
+        feedback = input(f'Is {guess} too high(H), too low(L), or correct(c)?? ').lower()
+        
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"yar! The computer guessed your number, {guess}, correctly!")
+
+computer_guess(100)
---
+++ madlibs.py
@@ -0,0 +1,10 @@
+adjText =input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous person: ")
+
+
+madlib = f"Computer programming is so {adjText}! It  makes me so excited all the times because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
